37c7185fc9c957f5dcdeb26721fa8616d99c0ce4
Teo Nogueira <teonogueira17@gmail.com>
2022-12-22 01:53:14 -0300

    finish

---
+++ assets/js/music.js
@@ -0,0 +1,34 @@
+let musics = [
+    { src:'audio/gsp2.mp3'},
+];
+
+let music = document.querySelector('audio');
+let indexMusic = 0;
+
+function renderMusic(){}
+
+document.querySelector('.btn-play').addEventListener('click', playMusic);
+
+document.querySelector('.btn-pause').addEventListener('click', pauseNow);
+music.addEventListener('timeupdate', actualize);
+
+
+
+function actualize(){}
+
+
+
+function playMusic(){
+    music.play();
+    document.querySelector('.btn-pause').style.display = 'block';
+    document.querySelector('.btn-play').style.display = 'none';
+  
+}
+
+
+function pauseNow(){
+    music.pause();
+    document.querySelector('.btn-pause').style.display = 'none';
+    document.querySelector('.btn-play').style.display = 'block';
+}
+
---
+++ assets/js/reveal_script.js
@@ -0,0 +1,50 @@
+
+
+        function stChange(a, old, nova) {
+        
+            a.classList.remove(old)
+            a.classList.add(nova)
+        }
+
+        setInterval(() => {
+            const doc = document.querySelector('#img');
+           stChange(doc, 'cb-0', 'cb-1');
+        }, 5000);
+       
+            
+
+    setInterval(() => {
+        const doc = document.querySelector('#img');
+       stChange(doc, 'cb-1', 'cb-2');
+    }, 4000);
+
+    setInterval(() => {
+        const doc = document.querySelector('#img');
+       stChange(doc, 'cb-2', 'cb-0');  
+}, 7000);
+
+
+
+    
+
+
+
+
+
+
+function revealed(a, b, c) {
+
+
+    return ScrollReveal().reveal(a, b, c)
+
+}
+
+revealed('.text-ava', {duration: 4000})
+revealed('.text-ava-2', {duration: 2000})
+revealed('.img-ava', {duration: 1500})
+revealed('.__h2', {duration: 2500})
+revealed('.__p', {duration: 2500})
+revealed('.border-01', {duration: 2000})
+revealed('.border-02', {duration: 2000})
+revealed('.list', {duration: 5000})
+revealed('.foot', {duration: 4000})
